5285f725005945079b6568cd67381aa97125fbac
EnderPrinter <ender3printer@mail.ru>
2022-05-08 18:29:04 +0500

    all done

--- main.py
+++ main.py
@@ -7,24 +7,25 @@ angle = 0
 anglePerStep = 0.176
 i = 1
 flag = 0
-# f = open("base.txt", "a")
-# f.write("angle;")
-# f.write("len;" + "\n")
-# f.close()
-while(1):
-    time.sleep(2)
-    for i in range (1, 2049):
-        ser.write('1'.encode('utf-8'))
-        angle = anglePerStep * i
-        #ser.write(i)
-        #time.sleep(0.04)
-        flag = 0
-        while(flag == 0):
-            if (ser.in_waiting > 0):
-                serialString = ser.readline()
-                print(serialString)
-                cm = int(serialString)
-                flag = 1
+f = open("base.txt", "w")
+f.write("angle;")
+f.write("len" + "\n")
+f.close()
+#while(1):
+time.sleep(2)
+for i in range (1, 1025):
+    ser.write('1'.encode('utf-8'))
+    angle = anglePerStep * i
+    #ser.write(i)
+    #time.sleep(0.04)
+    flag = 0
+    while(flag == 0):
+        if (ser.in_waiting > 0):
+            serialString = ser.readline()
+            print(serialString)
+            cm = int(serialString)
+            flag = 1
+
 
     # ser.write(b'1')  # write a string
     # time.sleep(1)
@@ -39,9 +40,11 @@ while(1):
     #     sinus = math.radians(angle)
     #     sinus = math.sin(angle)
     #     print(sinus)
-    # # f = open("base.txt", "a")
-    # # f.write(str(angle) + ";")
-    # # f.write(str(cm) + ";" + "\n")
-    # # f.close()
+    f = open("base.txt", "a")
+    #angle = int(angle)
+    f.write(str(angle) + ";")
+    f.write(str(cm) + "\n")
+    f.close()
     # time.sleep(0.15)
+ser.write('0'.encode('utf-8'))
 
