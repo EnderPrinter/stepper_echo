48111e8c87db64bd255f6ab5e20ef42a7c2d49e8
EnderPrinter <ender3printer@mail.ru>
2022-05-09 13:21:03 +0500

    all done edited

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import serial
 import time
-#import math
+
+# import math
+import numpy as np
+
+# create 1024 digit arrays
+MAXSIZE = 1025
+angles = np.empty(MAXSIZE)
+cms = np.empty(MAXSIZE)
 ser = serial.Serial('COM5', 9600, timeout=0)
 cm = 0
 angle = 0
 anglePerStep = 0.176
 i = 1
 flag = 0
+
+# begin database
 f = open("base.txt", "w")
 f.write("angle;")
 f.write("len" + "\n")
 f.close()
-#while(1):
-time.sleep(2)
-for i in range (1, 1025):
-    ser.write('1'.encode('utf-8'))
-    angle = anglePerStep * i
-    #ser.write(i)
-    #time.sleep(0.04)
-    flag = 0
-    while(flag == 0):
-        if (ser.in_waiting > 0):
-            serialString = ser.readline()
-            print(serialString)
-            cm = int(serialString)
-            flag = 1
 
+time.sleep(2)
+for i in range(1, 1025):
+    ser.write("1".encode("utf-8"))  # write 1 to arduino to do 1 step
+    angle = anglePerStep * i        # calculate angle
+    flag = 0
+    while (flag == 0):  # wait for answer
+        if (ser.in_waiting > 0):
+            serialString = ser.readline()  # receive len from  arduino's echo
+            # print(serialString)
+            cm = int(serialString)
+            flag = 1                       # update flag after we got everything
 
     # ser.write(b'1')  # write a string
     # time.sleep(1)
@@ -40,11 +46,14 @@ for i in range (1, 1025):
     #     sinus = math.radians(angle)
     #     sinus = math.sin(angle)
     #     print(sinus)
+    angles[i] = angle   # write data into array
+    cms[i] = cm         # same
+
+    # write data into database file
     f = open("base.txt", "a")
-    #angle = int(angle)
-    f.write(str(angle) + ";")
+    angle = str(angle)
+    f.write(angle + ";")
     f.write(str(cm) + "\n")
     f.close()
-    # time.sleep(0.15)
-ser.write('0'.encode('utf-8'))
-
+ser.write("0".encode("utf-8"))  # write 0 to do 180 back
+# print(np.sin(30))
